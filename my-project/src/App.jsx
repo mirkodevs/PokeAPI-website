203cd38c8fcce179f682e40aa5b161b14fcf9c7e
Mirko <mirkosaavedra596@gmail.com>
2024-01-05 20:21:10 +0100

    remove some pokemon

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -7,7 +7,7 @@ import "./index.css"
 function App() {
   const initialPokemonURL = "https://pokeapi.co/api/v2/pokemon?limit=40";
   const nonaGenerazioneURL = "https://pokeapi.co/api/v2/generation/9";
-  const allPokemonURL = "https://pokeapi.co/api/v2/pokemon?limit=1301";
+  const allPokemonURL = "https://pokeapi.co/api/v2/pokemon?limit=350";
   const [fetchingState, setFetchingState] = useState({
     hasUserPokemon: false,
     isFetching: true,
